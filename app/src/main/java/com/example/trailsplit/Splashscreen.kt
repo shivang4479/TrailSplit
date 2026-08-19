4f9b45ece82f7fbeaf99696a97cd6421559e1d8d
shivang sahu <sahushivang749@gmail.com>
2026-08-19 11:43:11 +0530

    splash screen added

--- app/src/main/java/com/example/trailsplit/Splashscreen.kt
+++ app/src/main/java/com/example/trailsplit/Splashscreen.kt
@@ -1,6 +1,9 @@
 package com.example.trailsplit
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,11 @@ class Splashscreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(this, MainActivity::class.java))
+            finish()
+        },5000)
 
         }
     }
