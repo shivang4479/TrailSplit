cb6dcf0b4154d9e22da096b60e51c3b8367d137d
shivang sahu <sahushivang749@gmail.com>
2026-08-19 08:37:53 +0530

    change

--- app/src/main/java/com/example/trailsplit/MainActivity.kt
+++ app/src/main/java/com/example/trailsplit/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.trailsplit
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val loginbtn=findViewById<Button>(R.id.btn)
+        loginbtn.setOnClickListener {
+            val intent= Intent(this, Signup::class.java)
+            startActivity(intent)
         }
+
+
     }
 }
