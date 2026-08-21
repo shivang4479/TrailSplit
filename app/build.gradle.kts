8e7723a38ae43af3ec03348ec2d53ddfb69a4d73
Abhay <abhaykumar99046@gmail.com>
2026-08-21 15:07:35 +0530

    homescreenworking_ktfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -38,8 +41,16 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.material)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+
 }
--- app/src/main/java/com/example/trailsplit/Homescreen.kt
+++ app/src/main/java/com/example/trailsplit/Homescreen.kt
@@ -3,6 +3,7 @@ package com.example.trailsplit
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Homescreen : AppCompatActivity() {
@@ -11,42 +12,9 @@ class Homescreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_homescreen)
+        val recyclerview = findViewById(R.id.expenserecycler)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
 
-        bottomNav.setOnItemSelectedListener { item ->
 
-            when (item.itemId) {
-
-                R.id.home_icon -> {
-                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
-                    true
-                }
-
-                R.id.trip_icon -> {
-                    Toast.makeText(this, "Trip", Toast.LENGTH_SHORT).show()
-                    true
-                }
-
-                R.id.add_icon -> {
-                    Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show()
-                    true
-                }
-
-                R.id.sync_icon -> {
-                    Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show()
-                    true
-                }
-
-                R.id.Profile_icon -> {
-                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
-                    true
-                }
-
-                else -> false
-            }
-        }
-
-        bottomNav.selectedItemId = R.id.home_icon
     }
 }
